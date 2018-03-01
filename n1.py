da0f2f4247dab361a3da6a353349699777a6ff38
tolik0 <iatsuk@ucu.edu.ua>
2018-03-01 14:27:23 +0200

    Minor updates

--- n1.py
+++ n1.py
@@ -7,7 +7,7 @@ def read_field(path):
     data = []
     with open(path, "r") as file:
         for line in file:
-            data.append(list(line[:-1]))
+            data.append(list(line.strip("\n")))
     return data
 
 
@@ -32,11 +32,13 @@ def ship_size(data, coor):
     length = 1
     for side in range(4):
         coor0 = coor
+        # choose what in what side we will go
         side_v, side_h = 0, 0
         if side == 0: side_v = 1
         if side == 1: side_v = -1
         if side == 2: side_h = 1
         if side == 3: side_h = -1
+        # check if there is ship in neighbour cell
         while 0 < coor0[0] + side_v < 10 and 0 < coor0[1] + side_h < 10 and \
                 data[coor0[0] + side_v][coor0[1] + side_h] == "*":
             length += 1
@@ -44,5 +46,40 @@ def ship_size(data, coor):
     return length
 
 
+def is_valid(data):
+    """
+    (data) -> (bool)
+    Check if board is correct
+    """
+    check = [0 for i in range(4)]
+    # calculate how many ships are with different lengths
+    for i in range(10):
+        for j in range(10):
+            length = ship_size(data, (chr(65 + j), i + 1))
+            # if ship is to big
+            if length > 4:
+                return False
+            # if there is ship there
+            if length:
+                check[length - 1] += 1
+    # check ships
+    for i in range(4):
+        if check[i] != (i + 1) * (4 - i):
+            return False
+    return True
+
+
+def field_to_str(data):
+    """
+    (data) -> (str)
+    Transform field to string
+    """
+    field = "  " + "".join([chr(65+i) for i in range(10)])
+    for i in range(10):
+        if i < 10:
+            field += "\n" + str(i+1) + " " + "".join(data[i])
+        else:
+            field += "\n" + str(i + 1) + "".join(data[i])
+    return field
 data = read_field("field.txt")
-print(ship_size(data, ("B", 2)))
+print(field_to_str(data))
